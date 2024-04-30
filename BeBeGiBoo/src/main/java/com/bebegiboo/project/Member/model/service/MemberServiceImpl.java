251b037c29e196a729f9f3f49dcf370bbd62b638
seungjoo1217 <rania2002@naver.com>
2024-04-30 14:23:38 +0900

    asf

---
+++ BeBeGiBoo/src/main/java/com/bebegiboo/project/Member/model/service/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bebegiboo.project.Member.model.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	@Override
+	public int checkId(String memberId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

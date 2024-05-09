c37d25babace5e949eabbef16343f267981dbf5d
vyoonv <vyoonheejv@gmail.com>
2024-05-09 22:16:44 +0900

    sdf

--- BeBeGiBoo/src/main/java/com/bebegiboo/project/manager/service/ManagerService.java
+++ BeBeGiBoo/src/main/java/com/bebegiboo/project/manager/service/ManagerService.java
@@ -2,25 +2,18 @@ package com.bebegiboo.project.manager.service;
 
 import java.util.List;
 
-<<<<<<< HEAD
-=======
 import com.bebegiboo.project.donateInfo.dto.DonationProduct;
->>>>>>> b0e1dfea78fcac5a7c2002b05087b270d1f19dfb
 import com.bebegiboo.project.member.model.dto.Member;
 
 public interface ManagerService {
 
-<<<<<<< HEAD
-	/** 회원 목록 출력
-=======
-	/** 회원 목록 조회
->>>>>>> b0e1dfea78fcac5a7c2002b05087b270d1f19dfb
+
+	/** 회원 목록 출력	
+
 	 * @return
 	 */
 	List<Member> selectMemberList();
 
-<<<<<<< HEAD
-=======
 	/** 회원 정보 수정
 	 * @param member
 	 * @return
@@ -32,5 +25,4 @@ public interface ManagerService {
 	 */
 	List<DonationProduct> selectDonationThingsList();
 
->>>>>>> b0e1dfea78fcac5a7c2002b05087b270d1f19dfb
 }
--- BeBeGiBoo/src/main/java/com/bebegiboo/project/mypage/model/service/UpdateMyInfoServiceImpl.java
+++ BeBeGiBoo/src/main/java/com/bebegiboo/project/mypage/model/service/UpdateMyInfoServiceImpl.java
@@ -1,7 +1,11 @@
 package com.bebegiboo.project.mypage.model.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.bebegiboo.project.member.model.dto.Member;
+import com.bebegiboo.project.mypage.model.mapper.UpdateMyInfoMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +13,29 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 @Service
 public class UpdateMyInfoServiceImpl implements UpdateMyInfoService{
+	
+	private final UpdateMyInfoMapper mapper; 
+	
+	private final BCryptPasswordEncoder bcrypt;
+	
+	/**
+	 * 비밀번호 체크 
+	 */
+	@Override
+	public int checkPw(Member loginMember) {
+		
+		String originPw = mapper.checkPw(loginMember.getMemberNo()); 
+		String inputPw = loginMember.getMemberPw();
+		
+		if(!bcrypt.matches(inputPw, originPw)) {
+			
+			return 0; 
+		}
+		
+		return 1;
+	}
 
+
+	
+	
 }
